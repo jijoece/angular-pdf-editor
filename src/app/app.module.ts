98905aec5fbe04e0399e789d0ce737b3357f9ae1
jijoece <jijoece@gmail.com>
2024-08-26 19:10:44 -0700

    incremental

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { PdfEditorComponent } from './pdf-editor/pdf-editor.component';
 import { SafeUrlPipe } from './safeurl';
 import { PdfCreatorComponent } from './pdf-creator/pdf-creator.component';
+import { NgxPdfEditorComponent } from './ngx-pdf-editor/ngx-pdf-editor.component';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { PdfCreatorComponent } from './pdf-creator/pdf-creator.component';
     PdfEditorComponent,
     SafeUrlPipe,
     PdfCreatorComponent,
+    NgxPdfEditorComponent,
     
   ],
   imports: [
